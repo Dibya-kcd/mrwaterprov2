9c63357b39405bcb897324ed0d2f1e3855699703
Dibya-kcd <dibyaprakashkcd@gmail.com>
2026-07-03 05:47:16 +0000

    Deploying to gh-pages from @ Dibya-kcd/mrwaterprov2@775b248c48371c10aac98a0f353eb88e3ea39ea5 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3268098111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "240313490" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
